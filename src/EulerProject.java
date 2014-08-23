0ef99b833d3bfb81a8d915912f9284db529ddddf
Jerry Woods <jerry@thecodesmith.com>
2014-08-23 12:38:41 -0500

    added scanner with beginning data validation

--- src/EulerProject.java
+++ src/EulerProject.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 
 public class EulerProject {
 
 	public static void main(String[] args){
-		System.out.println("Euler Project Represent!");
+		System.out.println("Euler Project Represent!\nWhich Euler Problem would you like the answer to?");
+		Scanner myScanner = new Scanner(System.in);
+		//displays error multiple times if multiple words are typed
+		while (!myScanner.hasNextInt()) {
+	        System.out.println("Please enter a valid number");
+	        myScanner.next();
+	    }
+	      int selectedProblem = myScanner.nextInt();
+	        System.out.println("The typed integer is " + selectedProblem);
+	     
+	     
+		
+		
+		
+/*		
 		int answer1 = 0;
 		for (int i=1; i<1000; i++){
 			if (i%3 == 0 || i%5 == 0) {
@@ -17,7 +33,9 @@ public class EulerProject {
 				answer2 += k;
 			}
 		}
+
 		System.out.println("The answer to problem #2 is " + answer2);
-	}
+*/	}
 	
 }
+// Checking to see if a save needs to be made in order for git to know a file has been modified
