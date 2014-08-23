2d7350ce36c4f1ab500ed0fc52deb79de24a5001
Jerry Woods <jerry@thecodesmith.com>
2014-08-22 20:35:48 -0500

    Added solution to Problem 1

--- src/EulerProject.java
+++ src/EulerProject.java
@@ -3,6 +3,13 @@ public class EulerProject {
 
 	public static void main(String[] args){
 		System.out.println("Euler Project Represent!");
+		int answer1 = 0;
+		for (int i=1; i<1000; i++){
+			if (i%3 == 0 || i%5 == 0) {
+				answer1 += i;
+			}
+		}
+		System.out.println("The answer to problem #1 is " + answer1);
 	}
 	
 }
